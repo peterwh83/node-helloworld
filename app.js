9849d243e33341c71696c09789d6c82b514e1542
peterwh83 <whgaos@cn.ibm.com>
2021-08-05 22:29:29 +0800

    argocd demo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8000
 
 app.get('/', (req, res) => {
-  res.send('tekton argocd demo No.4')
+  res.send('tekton argocd demo for Panorama')
 })
 
 app.listen(port, () => {
